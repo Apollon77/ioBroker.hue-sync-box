889601af6e929acebd8e9047b52ed80493705300
Issi <issi.dev.iobroker@gmail.com>
2022-11-27 09:10:24 +0100

    UI disabled because still under construction

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -40,9 +40,6 @@ const SettingsPageContent: React.FC = React.memo(() => {
 const migrateSettings = (settings: ioBroker.AdapterConfig) => {
 	// Here's an example for editing settings after they are loaded from the backend
 	// In this case, option1 will be set to true by default
-	if (settings.option1 === undefined) {
-		settings.option1 = true;
-	}
 };
 
 // Load your translations
